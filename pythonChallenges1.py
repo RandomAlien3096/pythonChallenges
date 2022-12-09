c5b899c44f1863db092d133716b038781a88e5c1
RandomAlien <100053214+RandomAlien3096@users.noreply.github.com>
2022-12-09 13:38:23 -0600

    Update pythonChallenges1.py

--- pythonChallenges1.py
+++ pythonChallenges1.py
@@ -50,3 +50,32 @@ if __name__ == '__main__':
     i=1
     for i in range(n):
         print(i+1, end="")
+        
+        
+"""
+A newly opened multinational brand has decided to base their company logo on the three most 
+common characters in the company name. They are now trying out various combinations of company 
+names and logos based on this condition. Given a string s, which is the company name in lowercase 
+letters, your task is to find the top three most common characters in the string.
+
+Print the three most common characters along with their occurrence count.
+Sort in descending order of occurrence count.
+If the occurrence count is the same, sort the characters in alphabetical order.
+"""
+import math
+import os
+import random
+import re
+import sys
+from collections import Counter
+
+
+if __name__ == '__main__':
+    s = input()
+    frequency = Counter(list(sorted(s)))
+    
+    for i,j in frequency.most_common(3):
+        print(i,j)
+        
+        
+ 
